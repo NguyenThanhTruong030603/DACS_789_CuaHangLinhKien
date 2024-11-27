8fe1de400cfceced081ad5e80943e48151f35c3b
NguyenThanhTruong030603 <145515640+NguyenThanhTruong030603@users.noreply.github.com>
2024-11-27 18:14:07 +0700

    hoàn thành chat

--- BaiGiuaKy/Hubs/ChatHub.cs
+++ BaiGiuaKy/Hubs/ChatHub.cs
@@ -7,7 +7,16 @@ namespace BaiGiuaky.Hubs
         // Hàm server nhận tin nhắn từ client và gửi lại cho tất cả các client
         public async Task SendMessage(string user, string message)
         {
-            await Clients.All.SendAsync("ReceiveMessage", user, message);
+            if (Context.User.Identity.IsAuthenticated)
+            {
+                // Nếu người dùng đã đăng nhập, gửi tin nhắn
+                await Clients.All.SendAsync("ReceiveMessage", user, message);
+            }
+            else
+            {
+                // Nếu người dùng chưa đăng nhập, gửi thông báo
+                await Clients.Caller.SendAsync("ReceiveMessage", "System", "Bạn cần đăng nhập để gửi tin nhắn.");
+            }
         }
     }
 }
